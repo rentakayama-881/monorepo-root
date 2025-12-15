d9f169460d9255ae2dbd3941a78f5d10a33b3c61
rentakayama-881 <radicalthinkers@protonmail.com>
2025-12-15 10:13:18 +0700

    fix: use useParams hook for thread details

--- backend/worker/event_worker.go
+++ backend/worker/event_worker.go
@@ -10,7 +10,7 @@ import (
     "github.com/ethereum/go-ethereum/ethclient"
     "github.com/ethereum/go-ethereum/common"
     "github.com/ethereum/go-ethereum/core/types"
-    "github.com/ethereum/go-ethereum/accounts/abi"
+     ethereum "github.com/ethereum/go-ethereum"
     // import your DB/model packages
 )
 
